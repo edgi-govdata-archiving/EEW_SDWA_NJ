02f7c0c7bf1137d8444a4b5adf700c6a96f15ae0
Eric Nost <ericnost@gmail.com>
2023-03-11 15:15:21 -0500

    map test

--- streamlit_test.py
+++ streamlit_test.py
@@ -2,13 +2,12 @@
 # Pick a place and get ECHO facilities
 #https://docs.streamlit.io/library/get-started/create-an-app 
 
-
+import pandas as pd
+import urllib.parse
 from hello import hello_world
 
-"""
 # Map
 from ipyleaflet import Map, Marker, basemaps, basemap_to_tiles
-
 m = Map(
     basemap=basemap_to_tiles(basemaps.CartoDB.Positron),
     center=(40, -74), 
@@ -18,25 +17,21 @@ m = Map(
 marker = Marker(location=(40, -74), draggable=True) # defaults to New Jersey for SDWA project
 m.add_layer(marker);
 
+display(m)
 
-sql= 'select * from "ECHO_EXPORTER" where "FAC_CITY" = \'PEEKSKILL\''
-
+# Goal = get marker, get facilities near marker, get violations 
 def load_data(sql):
   url= 'https://portal.gss.stonybrook.edu/echoepa/?query='
   data_location = url + urllib.parse.quote_plus(sql) + '&pg'
   data = pd.read_csv(data_location, encoding='iso-8859-1', dtype={"REGISTRY_ID": "Int64"})
-  data["LAT"] = data["FAC_LAT"]
-  data["LON"] = data["FAC_LONG"]
+  #data["LAT"] = data["FAC_LAT"]
+  #data["LON"] = data["FAC_LONG"]
+  # add data to map
   return data
-
+sql= 'select * from "ECHO_EXPORTER" where "FAC_CITY" = \'PEEKSKILL\''
 
 # Streamlit section
 import streamlit as st
-#data_load_state = st.text('Loading data...')
-#data = load_data(sql)
-#data_load_state.text('Loading data...done!')
-
-m
 """
 import streamlit as st
 data_load_state = st.text('Loading data...')
