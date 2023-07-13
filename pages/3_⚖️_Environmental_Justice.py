cd7a24390608b78a44affcb3c12aaced6333f758
Eric Nost <ericnost@gmail.com>
2023-07-13 16:16:17 -0400

    add more explanation to EJ variables
    Fixes #47
    Fixes #30

--- pages/3_⚖️_Environmental_Justice.py
+++ pages/3_⚖️_Environmental_Justice.py
@@ -41,8 +41,6 @@ st.markdown("""
 Use the dropdown menu below to select an EJ measure to study. The map will change to show each of the Census block groups that are at least partly in the selected area, and
 the recorded value for the EJ measure there. The data come from EPA's EJScreen tool.
 
-*Important: any percentages shown here are shown as decimals. For instance, 80% is shown as .8*
-
 On the map, the darker the shade of the blue, 
 the more of the measure - a higher percentage minority population, for instance.
 
@@ -72,14 +70,76 @@ def add_spatial_data(url, name, projection=4326):
   sd.to_crs(crs=projection, inplace=True) # transform to input projection, defaults to WGS GCS
   return sd
 
+# EJ parameters we are working with
+ej_parameters = {
+  "MINORPCT",
+  "LOWINCPCT",
+  "LESSHSPCT",
+  "LINGISOPCT",
+  "UNDER5PCT",
+  "OVER64PCT",
+  #"PRE1960PCT",
+  "UNEMPPCT",
+  #"VULEOPCT",
+  #"DISPEO",
+  #"DSLPM",
+  #"CANCER",
+  #"RESP",
+  #"PTRAF",
+  #"PWDIS",
+  #"PNPL",
+  #"PRMP",
+  #"PTSDF",
+  #"OZONE",
+  #"PM25",
+  #"UST"
+}
+
+@st.cache_data
+def get_metadata():
+  columns = pd.read_csv("https://raw.githubusercontent.com/edgi-govdata-archiving/ECHO-SDWA/main/2021_EJSCREEEN_columns-explained.csv")
+  return columns
+columns = get_metadata()
+columns = columns.loc[columns["GDB Fieldname"].isin(ej_parameters)][["GDB Fieldname", "Description"]]
+columns.set_index("Description", inplace = True)
+ej_dict = columns.to_dict()['GDB Fieldname']
+#options = ej_dict.keys() # list of EJScreen variables that will be selected
+ejdefs = {
+  "MINORPCT": "Percent of individuals in a block group who list their racial status as a race other than white alone and/or list their ethnicity as Hispanic or Latino. That is, all people other than non-Hispanic white-alone individuals. The word 'alone' in this case indicates that the person is of a single race, not multiracial.",
+  "LOWINCPCT": "Percent of a block group's population in households where the household income is less than or equal to twice the federal poverty level.",
+  "LESSHSPCT": "Percent of people age 25 or older in a block group whose education is short of a high school diploma.",
+  "LINGISOPCT": "Percent of people in a block group living in limited English speaking households. A household in which all members age 14 years and over speak a non-English language and also speak English less than 'very well' (have difficulty with English) is limited English speaking.",
+  "UNDER5PCT": 'Percent of people in a block group under the age of 5.',
+  "OVER64PCT": 'Percent of people in a block group over the age of 64.',
+  #"PRE1960PCT": "Percent of housing units built pre-1960, as indicator of potential lead paint exposure",
+  "UNEMPPCT": "Percent of a block group's population that did not have a job at all during the reporting period, made at least one specific active effort to find a job during the prior 4 weeks, and were available for work (unless temporarily ill).",
+  #"VULEOPCT": "Demographic index of block group used by EPA, an average of low income and people of color populations",
+  #"DISPEO": "Intermediate variable used for calculation of demographic index",
+  #"DSLPM": "Diesel particulate matter level in air, µg/m3",
+  #"CANCER": "Lifetime cancer risk from inhalation of air toxics",
+  #"RESP": "Ratio of exposure concentration to health-based reference concentration",
+  #"PTRAF": "Count of vehicles (AADT, avg. annual daily traffic) at major roads within 500 meters, divided by distance in meters (not km)",
+  #"PWDIS": "RSEI modeled toxic concentrations at stream segments within 500 meters, divided by distance in kilometers (km)",
+  #"PNPL": "Count of proposed or listed NPL - also known as superfund - sites within 5 km (or nearest one beyond 5 km), each divided by distance in kilometers",
+  #"PRMP": "Count of RMP (potential chemical accident management plan) facilities within 5 km (or nearest one beyond 5 km), each divided by distance in kilometers",
+  #"PTSDF": "Count of hazardous waste facilities (TSDFs and LQGs) within 5 km (or nearest beyond 5 km), each divided by distance in kilometers",
+  #"OZONE": "Annual average of top ten maximum daily 8-hour ozone air concentrations in parts per billion",
+  #"PM25": "PM2.5 levels in air, µg/m3 annual avg.",
+  #"UST": "Count of leaking underground storage tanks (multiplied by a factor of 7.7) and the number of underground storage tanks within a 1,500-foot buffered block group"
+}
+options = ejdefs.values()
+
 # Load and join census data
 with st.spinner(text="Loading data..."):
   census_data = add_spatial_data(url="https://www2.census.gov/geo/tiger/TIGER2017/BG/tl_2017_34_bg.zip", name="census") #, projection=4269
   ej_data = pd.read_csv("https://github.com/edgi-govdata-archiving/ECHO-SDWA/raw/main/EJSCREEN_2021_StateRankings_NJ.csv") # NJ specific
-  ej_data["ID"] = ej_data["ID"].astype(str)
-  census_data.set_index("GEOID", inplace=True)
-  ej_data.set_index("ID", inplace=True)
-  census_data = census_data.join(ej_data)
+  ej_data["ID"] = ej_data["ID"].astype(str) # set the Census id as a string
+  census_data.set_index("GEOID", inplace=True) # set it to the index in the Census data
+  ej_data.set_index("ID", inplace=True) # set the Census id to the index in the EJScreen data
+  census_data = census_data.join(ej_data) # join based on this shared id
+  census_data.rename(columns=ejdefs, inplace=True) # rename columns to the EJ variable definitions
+  census_data = census_data[[i for i in ejdefs.values()] + ["geometry"]] # Filter out unnecessary columns
+  census_data[[i for i in ejdefs.values()]] = round(census_data[[i for i in ejdefs.values()]] * 100, 2) # Convert decimal values to percentages
 
 # Convert st.session_state["last_active_drawing"]
 try:
@@ -115,73 +175,16 @@ def main():
   c2 = st.container()
 
   with c2:
-    # EJ parameters we are working with
-    ej_parameters = {
-      "MINORPCT",
-      "LOWINCPCT",
-      "LESSHSPCT",
-      "LINGISOPCT",
-      "UNDER5PCT",
-      "OVER64PCT",
-      #"PRE1960PCT",
-      "UNEMPPCT",
-      #"VULEOPCT",
-      #"DISPEO",
-      #"DSLPM",
-      #"CANCER",
-      #"RESP",
-      #"PTRAF",
-      #"PWDIS",
-      #"PNPL",
-      #"PRMP",
-      #"PTSDF",
-      #"OZONE",
-      #"PM25",
-      #"UST"
-    }
-
-    @st.cache_data
-    def get_metadata():
-      columns = pd.read_csv("https://raw.githubusercontent.com/edgi-govdata-archiving/ECHO-SDWA/main/2021_EJSCREEEN_columns-explained.csv")
-      return columns
-    columns = get_metadata()
-    columns = columns.loc[columns["GDB Fieldname"].isin(ej_parameters)][["GDB Fieldname", "Description"]]
-    columns.set_index("Description", inplace = True)
-    ej_dict = columns.to_dict()['GDB Fieldname']
-
-    options = ej_dict.keys() # list of EJScreen variables that will be selected
-    st.markdown("# Which EJ measure shall we explore?")
     ejdesc = st.selectbox(
-      "Which EJ measure shall we explore?",
-      options,
+      label = "Which EJ measure shall we explore?",
+      options = options,
+      format_func = lambda x: x[0:100]+"...", # Only show the first 100 characters of the variable
       label_visibility = "hidden"
     )
-    ejvar = ej_dict[ejdesc]
-    ejdefs = {
-      "MINORPCT": "Percent of individuals in a block group who list their racial status as a race other than white alone and/or list their ethnicity as Hispanic or Latino. That is, all people other than non-Hispanic white-alone individuals. The word 'alone' in this case indicates that the person is of a single race, not multiracial.",
-      "LOWINCPCT": "Percent of a block group's population in households where the household income is less than or equal to twice the federal poverty level.",
-      "LESSHSPCT": "Percent of people age 25 or older in a block group whose education is short of a high school diploma.",
-      "LINGISOPCT": "Percent of people in a block group living in limited English speaking households. A household in which all members age 14 years and over speak a non-English language and also speak English less than 'very well' (have difficulty with English) is limited English speaking.",
-      "UNDER5PCT": 'Percent of people in a block group under the age of 5.',
-      "OVER64PCT": 'Percent of people in a block group over the age of 64.',
-      #"PRE1960PCT": "Percent of housing units built pre-1960, as indicator of potential lead paint exposure",
-      "UNEMPPCT": "Percent of a block group's population that did not have a job at all during the reporting period, made at least one specific active effort to find a job during the prior 4 weeks, and were available for work (unless temporarily ill).",
-      #"VULEOPCT": "Demographic index of block group used by EPA, an average of low income and people of color populations",
-      #"DISPEO": "Intermediate variable used for calculation of demographic index",
-      #"DSLPM": "Diesel particulate matter level in air, µg/m3",
-      #"CANCER": "Lifetime cancer risk from inhalation of air toxics",
-      #"RESP": "Ratio of exposure concentration to health-based reference concentration",
-      #"PTRAF": "Count of vehicles (AADT, avg. annual daily traffic) at major roads within 500 meters, divided by distance in meters (not km)",
-      #"PWDIS": "RSEI modeled toxic concentrations at stream segments within 500 meters, divided by distance in kilometers (km)",
-      #"PNPL": "Count of proposed or listed NPL - also known as superfund - sites within 5 km (or nearest one beyond 5 km), each divided by distance in kilometers",
-      #"PRMP": "Count of RMP (potential chemical accident management plan) facilities within 5 km (or nearest one beyond 5 km), each divided by distance in kilometers",
-      #"PTSDF": "Count of hazardous waste facilities (TSDFs and LQGs) within 5 km (or nearest beyond 5 km), each divided by distance in kilometers",
-      #"OZONE": "Annual average of top ten maximum daily 8-hour ozone air concentrations in parts per billion",
-      #"PM25": "PM2.5 levels in air, µg/m3 annual avg.",
-      #"UST": "Count of leaking underground storage tanks (multiplied by a factor of 7.7) and the number of underground storage tanks within a 1,500-foot buffered block group"
-    }
+    ejvar = ejdesc #ej_dict[ejdesc]
+
     st.markdown("**EPA defines this as:**")
-    st.markdown(ejdefs[ejvar])
+    st.markdown(ejvar)
     st.caption("Source for definitions of environmental justice indicators: [socioeconomic](https://www.epa.gov/ejscreen/overview-socioeconomic-indicators-ejscreen) | [environmental](https://www.epa.gov/ejscreen/overview-environmental-indicators-ejscreen)")
     st.markdown(":arrow_right: What assumptions are built into EPA's choices and definitions of environmental justice indicators?")
 
